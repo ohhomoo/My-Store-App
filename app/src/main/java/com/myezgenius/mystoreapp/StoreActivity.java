d8cdce51b9f98323291e160fd6d7e70aa51011eb
ohhomoo <ohho7519@gmail.com>
2016-05-29 16:13:09 +0700

    Project Successful

--- app/src/main/java/com/myezgenius/mystoreapp/StoreActivity.java
+++ app/src/main/java/com/myezgenius/mystoreapp/StoreActivity.java
@@ -1,13 +1,20 @@
 package com.myezgenius.mystoreapp;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -15,6 +22,14 @@ import com.squareup.okhttp.Response;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class StoreActivity extends AppCompatActivity {
 
@@ -36,6 +51,82 @@ public class StoreActivity extends AppCompatActivity {
 
 
     }   //Main Method
+
+    class InstallTask extends AsyncTask<Void, Void, String> {
+        ProgressDialog mProgressDialog;
+
+        Context context;
+        String url;
+
+        public InstallTask(Context context, String url) {
+            this.context = context;
+
+            this.url = url;
+
+        }
+
+        protected void onPreExecute() {
+            mProgressDialog = ProgressDialog.show(context,
+                    "Download", " Downloading in progress..");
+        }
+
+        private String downloadapk() {
+            String result = "";
+            try {
+                URL url = new URL(this.url);
+                HttpURLConnection urlConnection = (HttpURLConnection) url
+                        .openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setDoOutput(true);
+                urlConnection.connect();
+
+                File sdcard = Environment.getExternalStorageDirectory();
+                File file = new File(sdcard, "filename.apk");
+
+                FileOutputStream fileOutput = new FileOutputStream(file);
+                InputStream inputStream = urlConnection.getInputStream();
+
+                byte[] buffer = new byte[1024];
+                int bufferLength = 0;
+
+                while ((bufferLength = inputStream.read(buffer)) > 0) {
+                    fileOutput.write(buffer, 0, bufferLength);
+                }
+                fileOutput.close();
+                result = "done";
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        protected String doInBackground(Void... params) {
+            String result = downloadapk();
+            return result;
+        }
+
+        protected void onPostExecute(String result) {
+            if (result.equals("done")) {
+                mProgressDialog.dismiss();
+                installApk();
+            } else {
+                Toast.makeText(context, "Error while downloading",
+                        Toast.LENGTH_LONG).show();
+
+            }
+        }
+
+        private void installApk() {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            Uri uri = Uri.fromFile(new File("/sdcard/filename.apk"));
+            intent.setDataAndType(uri, "application/vnd.android.package-archive");
+            context.startActivity(intent);
+        }
+
+    }
 
     public class ConnectedStore extends AsyncTask<Void, Void, String> {
         @Override
@@ -112,6 +203,16 @@ public class StoreActivity extends AppCompatActivity {
     private void myIstallApp(String nameString, String apkString) {
         urlAPK = apkString;
         Log.d("StoreV3", "urlAPK ==> " + urlAPK);
+
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        InstallTask installTask = new InstallTask(StoreActivity.this, urlAPK);
+        installTask.execute();
+
+        Toast.makeText(this,"Install" + nameString + "Successful",
+                Toast.LENGTH_SHORT).show();;
 
     }   // myInstall
 
