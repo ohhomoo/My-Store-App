c6a75925f6364263155f12029cb41c3747b61047
ohhomoo <ohho7519@gmail.com>
2016-05-29 12:39:58 +0700

    Synchronize mySQL to SQLite Finish

--- app/src/main/java/com/myezgenius/mystoreapp/MainActivity.java
+++ app/src/main/java/com/myezgenius/mystoreapp/MainActivity.java
@@ -1,14 +1,25 @@
 package com.myezgenius.mystoreapp;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private MyManage myManage;
+    private static final String urlJSON = "http://swiftcodingthai.com/Moo/get_user_moo.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +28,80 @@ public class MainActivity extends AppCompatActivity {
 
         myManage = new MyManage(this);
 
+        //myManage.addUser("name", "surname", "user", "pass");
+
+        deleteAllSQLite();
+
+        synMySQLtoSQLite();
+
     }   // Main Method
+
+    // Create Inner Class
+    public class ConnectedUserJSON extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                Log.d("Store", "myerror = " + e.toString());
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String strJSON) {
+            super.onPostExecute(strJSON);
+
+            Log.d("Store", "strJSON ==> " + strJSON);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(strJSON);
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String strName = jsonObject.getString(MyManage.column_name);
+                    String strSurname = jsonObject.getString(MyManage.column_surname);
+                    String strUser = jsonObject.getString(MyManage.column_user);
+                    String strPassword = jsonObject.getString(MyManage.column_password);
+                    myManage.addUser(strName, strSurname, strUser, strPassword);
+
+                }   //for
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }   // onPost
+
+
+
+    }   // Connected Class
+
+
+    private void synMySQLtoSQLite() {
+        ConnectedUserJSON connectedUserJSON = new ConnectedUserJSON();
+        connectedUserJSON.execute();
+
+
+    }
+
+    private void deleteAllSQLite() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+    }
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
--- app/src/main/java/com/myezgenius/mystoreapp/MyManage.java
+++ app/src/main/java/com/myezgenius/mystoreapp/MyManage.java
@@ -1,5 +1,6 @@
 package com.myezgenius.mystoreapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,11 +13,35 @@ public class MyManage {
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase sqLiteDatabase;
 
+    public static final String user_table = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_name = "Name";
+    public static final String column_surname = "Surname";
+    public static final String column_user = "User";
+    public static final String column_password = "Password";
+
+
     public MyManage(Context context) {
 
         myOpenHelper = new MyOpenHelper(context);
         sqLiteDatabase = myOpenHelper.getWritableDatabase();
 
     }   //Constructor
+
+    public long addUser(String strName,
+                        String strSurname,
+                        String strUser,
+                        String strPassword) {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_name, strName);
+        contentValues.put(column_surname, strSurname);
+        contentValues.put(column_user, strUser);
+        contentValues.put(column_password, strPassword);
+
+        return sqLiteDatabase.insert(user_table, null, contentValues);
+    }
+
+
 
 }   // MyManage Class
